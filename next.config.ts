e8611782d8568954e7dcc165de3e2335f0abbbdb
jeonseun <jeonseun1234@gmail.com>
2025-06-21 14:02:00 +0900

    chore: next.js static 빌드 설정

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/study-gh-pages',
+  output: 'export',
 };
 
 export default nextConfig;
