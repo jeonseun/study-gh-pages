e6e2f13e0d3fb2dd736cd72458f8463be3d9e8ad
jeonseun <jeonseun1234@gmail.com>
2025-06-21 13:50:42 +0900

    feat: next.js basePath 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/study-gh-pages',
 };
 
 export default nextConfig;
