ce8641fc83589e0b8c3dd2738f11672a654ed915
jeonseun <jeonseun1234@gmail.com>
2025-06-21 10:48:30 +0900

    init: 배포 테스트용 nextjs 프로젝트 생성

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+import './globals.css';
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
