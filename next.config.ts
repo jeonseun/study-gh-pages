1127ebe5dafc261c0f2e3e31c67ba350fda060ef
jeonseun <jeonseun1234@gmail.com>
2025-06-22 11:19:16 +0900

    test: next.js config 값 제거

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/study-gh-pages',
-  output: 'export',
-  trailingSlash: true,
+  // basePath: '/study-gh-pages'
+  // output: 'export',
+  // trailingSlash: true,
 };
 
 export default nextConfig;
