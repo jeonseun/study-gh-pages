6a8f1e227c0faba77c50845f3c7c7905c48db416
jeonseun <jeonseun1234@gmail.com>
2025-06-22 12:10:27 +0900

    test: 환경변수 세팅없이 읽어보기

--- src/app/abc/page.tsx
+++ src/app/abc/page.tsx
@@ -1,10 +1,21 @@
 import Link from 'next/link';
 
 export default function Page() {
+  const envVars = {
+    repository: process.env.GITHUB_REPOSITORY,
+    sha: process.env.GITHUB_SHA,
+    workflow: process.env.GITHUB_WORKFLOW,
+    runId: process.env.GITHUB_RUN_ID,
+    ref: process.env.GITHUB_REF,
+    action: process.env.GITHUB_ACTION,
+  };
+
   return (
     <>
       <h1>hello page</h1>
       <Link href={'/'}>link</Link>
+      <span></span>
+      {envVars.repository}
     </>
   );
 }
